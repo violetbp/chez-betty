9842a48c4f2f8893604bb96a9f0d29b073f3ed79
Violet Boyse-Peacor <violetbp@eecs.umich.edu>
2020-03-09 13:04:14 -0400

    update

--- runthis.sh
+++ runthis.sh
@@ -1,6 +1,18 @@
+mkdir -p chezbetty/static/css/lib/bootstrap/ chezbetty/static/js/lib/bootstrap/ chezbetty/static/js/lib/jquery/
+mkdir -p 
 
-cp ./node_modules/bootstrap/dist/css/bootstrap.min.css static/css/lib/bootstrap/bootstrap.min.css
-cp  static/css/lib/jquery-ui/jquery-ui.min.css
-cp ./node_modules/bootstrap/dist/js/bootstrap.min.js static/js/lib/bootstrap/bootstrap.min.js
-cp  static/js/lib/jquery-ui/jquery-ui.min.js
-cp ./node_modules/jquery/dist/jquery.min.js static/js/lib/jquery/jquery.min.js
+cp ./node_modules/bootstrap/dist/css/bootstrap.min.css chezbetty/static/css/lib/bootstrap/bootstrap.min.css
+cp ./node_modules/bootstrap/dist/js/bootstrap.min.js chezbetty/static/js/lib/bootstrap/bootstrap.min.js
+cp ./node_modules/jquery/dist/jquery.min.js chezbetty/static/js/lib/jquery/jquery.min.js
+
+
+mkdir temp
+pushd temp
+wget https://jqueryui.com/resources/download/jquery-ui-1.12.1.zip
+unzip jquery-ui-1.12.1.zip
+pushd jquery-ui-1.12.1
+mkdir -p chezbetty/static/js/lib/jquery-ui/
+cp  jquery-ui.min.css ../..chezbetty/static/css/lib/jquery-ui/jquery-ui.min.css
+cp  jquery-ui.min.js ../..chezbetty/static/js/lib/jquery-ui/jquery-ui.min.js
+popd
+popd
--- viviinstall2.sh
+++ viviinstall2.sh
@@ -1,9 +1,14 @@
-sudo apt install apache2 build-essential certbot debhelper emacs enchant fail2ban fonts-ubuntu-console git htop ldap-utils libapache2-mod-wsgi-py3 libjpeg-dev nodejs ntp python3-all-dbg python3-all-dev python3-doc python3-pip python3-venv python-all-dbg python-all-dev python-certbot-apache python-doc python-egenix-mx-base-dev python-ldap python-sphinx quilt sasl2-bin sendmail systemd-sysv tcsh units zsh zsh-doc npm postgresql postgresql-server-dev-10 libjpeg-dev -y
+sudo apt install apache2 build-essential certbot debhelper emacs enchant fail2ban fonts-ubuntu-console git htop ldap-utils libapache2-mod-wsgi-py3 libjpeg-dev ntp python3-all-dbg python3-all-dev python3-doc python3-pip python3-venv python-all-dbg python-all-dev python-certbot-apache python-doc python-egenix-mx-base-dev python-ldap python-sphinx quilt sasl2-bin sendmail systemd-sysv tcsh units zsh zsh-doc npm postgresql postgresql-server-dev-10 libjpeg-dev -y
 sudo apt install python3.7-dev python3.7-venv sqlite3 -y
 
 git clone git@github.com:violetbp/chez-betty.git
 
 cd chez-betty
+
+#install node
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 
 
 python3.7 -m venv env
@@ -16,10 +21,13 @@ pip uninstall -y zope.interface zope.sqlalchemy
 pip install zope.deprecation==4.4.0 zope.interface==4.6.0 zope.sqlalchemy==1.1
 
 
+mkdir chezbetty/static/js/lib
+
 # sudo npm install bower bower-installer -g
 # bower-installer
 #using yarn now
-sudo npm install yarn
+#yarn
+sudo npm i yarn -g
 yarn
 
 # Database stuff 
@@ -29,6 +37,8 @@ path=${path//\//\\\/} #replace the backslashes with escaped ones
 sed "s/sqlalchemy.url = sqlite:\/\/\/%(here)s\/chezbetty.sqlite/$path/" development.ini.example >development.ini
 #run initialziation script
 python chezbetty/initializedb.py development.ini
+
+
 
 echo "source env/bin/activate"
 echo "pserve development.ini"
